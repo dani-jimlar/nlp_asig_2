17981a4c24b3fa0a67e4b029dac20f0a4dfd942e
dani-jimlar <daniela.jimenezlara@duke.edu>
2023-09-27 22:05:06 -0400

    update py

--- unigram_pytorch.py
+++ unigram_pytorch.py
@@ -50,7 +50,7 @@ class Unigram(nn.Module):
         p = normalize(torch.sigmoid(self.s))
 
         # compute log probability of input
-        return torch.sum(input, 1, keepdim=True).T @ torch.log(p)
+        return torch.sum(input, 1, keepdim=True).T @ torch.log(p), p
 
 
 def gradient_descent_example():
@@ -76,27 +76,70 @@ def gradient_descent_example():
 
     # set number of iterations and learning rate
     num_iterations = 100
-    learning_rate = 0.1
+    learning_rate = 0.02
 
     # train model
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     list_loss = []
     list_items = []
     for i in range(num_iterations):
-        p_pred = model(x)
+        p_pred, p = model(x)
         loss = -p_pred
         loss.backward(retain_graph=True)
         optimizer.step()
         optimizer.zero_grad()
         list_loss.append(i)
         list_items.append(loss.item())
+    true_p = torch.sum(x, 1, keepdim=True) / len(text)
+    mm = -(torch.sum(x, 1, keepdim=True)).T @ torch.log(true_p)
+    min = mm.item()
+    # graph 1
+    plt.figure()
 
     plt.plot(list_loss, list_items)
+    plt.xlabel("iterations")
+    plt.ylabel("loss")
+    plt.title("Loss as a function of time ")
+    plt.axhline(y=min, color="red", linestyle="--", label="Minimum possible loss")
+    plt.legend()
     plt.show()
+    plt.savefig("g_1_100.png", dpi=300, bbox_inches="tight")
 
-    # display results
-    # raise RuntimeError("Remove this error and create visualizations.")  # DO THIS
-    # graph
+    # graph 2
+    plt.figure()
+
+    plt.scatter(
+        [str(i) for i in vocabulary],
+        true_p.squeeze().tolist(),
+        color="blue",
+        alpha=0.4,
+        label="Token probabilites",
+    )
+    plt.scatter(
+        [str(i) for i in vocabulary],
+        p.squeeze().tolist(),
+        color="orange",
+        alpha=1,
+        marker="^",
+        label="known probabilites",
+    )
+    plt.xlabel("Vocabulary")
+    plt.ylabel("Probability")
+    plt.title("Final token probabilities")
+    plt.legend()
+    plt.show
+    plt.savefig("g_2_100.png", dpi=300, bbox_inches="tight")
+
+    # prob differences
+    plt.figure()
+    difference_list = [x - y for x, y in zip(true_p, p)]
+    values_list = [tensor.item() for tensor in difference_list]
+    plt.scatter([str(i) for i in vocabulary], values_list, color="green")
+    plt.title("Difference in probabilities")
+    plt.xlabel("Vocabulary")
+    plt.ylabel("Difference in probability")
+    plt.show
+    plt.savefig("g_3_100.png", dpi=300, bbox_inches="tight")
 
 
 if __name__ == "__main__":
